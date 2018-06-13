ca8ffcc96a2b03544b8381778db324e38d8d512e
LvChengbin <lvchengbin@nextseason.cc>
2018-06-14 00:51:13 +0800

    upgrade

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p "$HOME/.tox" && \
-git clone https://github.com/LvChengbin/tox.git ~/.tox && \
-echo "source ~/.tox/tox.sh" >> "~/.bashrc" && \
+curl https://raw.githubusercontent.com/LvChengbin/tox/master/tox.sh -o "$HOME/.tox/tox.sh" && \
+[ -f "$HOME/.bashrc" ] || touch "$HOME/.bashrc" && \
+echo "source ~/.tox/tox.sh" >> "$HOME/.bashrc" && \
+source $HOME/.bashrc && \
 toxc
